633175f5cd3fd8f4424408acb73609c86c0f5b93
raymond-lau-lyf <raymond.lau.lyf@gmail.com>
2025-12-23 15:29:58 +0800

    Update the CMake configuration to use C++17 and modify the IP address in the MID360 configuration.

--- build.sh
+++ build.sh
@@ -55,10 +55,10 @@ fi
 pushd `pwd` > /dev/null
 if [ $ROS_VERSION = ${VERSION_ROS1} ]; then
     cd ../../
-    catkin_make -DROS_EDITION=${VERSION_ROS1}
+    catkin_make -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DROS_EDITION=${VERSION_ROS1}
 elif [ $ROS_VERSION = ${VERSION_ROS2} ]; then
     cd ../../
-    colcon build --cmake-args -DROS_EDITION=${VERSION_ROS2} -DHUMBLE_ROS=${ROS_HUMBLE}
+    colcon build --cmake-args -DCMAKE_POLICY_VERSION_MINIMUM=3.5 -DROS_EDITION=${VERSION_ROS2} -DHUMBLE_ROS=${ROS_HUMBLE}
 fi
 popd > /dev/null
 
